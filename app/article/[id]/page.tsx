16ac0b15f61853845f1227e4ef6e711e37e5cac2
Merto <mertenercan@gmail.com>
2023-08-29 23:35:19 +0300

    - created article route for dynamic articles

---
+++ app/article/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface ArticlePageProps {
+  params:{ id: string }
+}
+
+const ArticlePage: FC<ArticlePageProps> = ({ params }) => {
+  return (
+    <main>
+        <h1 className='text-black text-4xl font-bold'>{`Article id:${params.id}`}</h1>
+    </main>
+  )
+}
+
+export default ArticlePage;
