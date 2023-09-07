c722fc7a147c2cb8bfa2a1e3de49e2a5370d9a40
Merto <mertenercan@gmail.com>
2023-09-07 23:40:59 +0300

    - minor refactor

--- components/Forms/LoginForm/index.tsx
+++ components/Forms/LoginForm/index.tsx
@@ -10,7 +10,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 interface LoginFormProps {}
 const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     'http://localhost:3000/';
 
