c7d2326aad5ca77e2034371466ce12c9e25f8696
Merto <mertenercan@gmail.com>
2023-09-10 17:08:47 +0300

    - dynamic server config

--- app/api/products/getAll/route.ts
+++ app/api/products/getAll/route.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-
+export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
     
     try{
--- app/api/products/getSingle/route.ts
+++ app/api/products/getSingle/route.ts
@@ -5,6 +5,7 @@ import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
+export const dynamic = 'force-dynamic'
 
 export async function POST(request: NextRequest) {
     const {id} = await request.json()
