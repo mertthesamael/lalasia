19bdd28b65ae03e09a55ad6a56ebba7086fda0bb
merto <mertenercan@gmail.com>
2023-09-12 22:24:33 +0000

    -minor refactor

--- app/products/loading.tsx
+++ app/products/loading.tsx
@@ -13,7 +13,7 @@ const Loading: FC<LoadingProps> = ({  }) => {
       <section className='w-full flex items-center gap-6 justify-center'>
       <div className='flex flex-col items-center py-20 justify-center md:justify-between md:flex-row flex-wrap  gap-2 md:gap-5 w-full max-w-screen-xl mx-5 md:mx-20  md:px-0'>
         {Array(4).fill(0).map((el,_i) => (
-          <div className='w-[15rem] h-max flex flex-col justify-center items-center flex-wrap gap-6 p-4 '>
+          <div key={_i} className='w-[15rem] h-max flex flex-col justify-center items-center flex-wrap gap-6 p-4 '>
             <div className='w-full h-[10rem] bg-gray-400 animate-pulse'></div>
             <div className='w-full h-[2rem] bg-gray-400 animate-pulse'></div>
             <div className='w-full h-[2rem] bg-gray-400 animate-pulse'></div>
