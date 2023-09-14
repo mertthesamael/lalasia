a6cce99fdc1736a87f94d529023e353dda5924e4
Merto <mertenercan@gmail.com>
2023-09-14 23:54:06 +0300

    - minor refactor

--- containers/product-page/related-section/index.tsx
+++ containers/product-page/related-section/index.tsx
@@ -1,17 +1,24 @@
 import ProductCard from "@/components/Cards/ProductCard";
+import { getAll } from "@/libs/endpoints";
+import { TProduct } from "@/types/Product";
+import axios from "axios";
 import React, { FC } from "react";
 
 interface RelatedSectionProps {}
-
-const RelatedSection: FC<RelatedSectionProps> = ({}) => {
+const getItems = async () => {
+  const { data } = await axios(getAll);
+  return data;
+};
+const RelatedSection: FC<RelatedSectionProps> = async({}) => {
+  const items = await getItems()
   return (
     <section className="w-full flex justify-center">
-      {/* <div className='flex flex-col justify-between  gap-7 w-full max-w-screen-xl mx-5 md:mx-20 py-10'>
+      /* <div className='flex flex-col justify-between  gap-7 w-full max-w-screen-xl mx-5 md:mx-20 py-10'>
         <h1 className='text-black text-3xl font-bold'>Related Items</h1>
         <div className='flex w-full flex-wrap gap-6 justify-between'>
-            {Array(3).fill(0).map((el, _i) => <ProductCard key={_i}/>)}
+            {items.data.splice(1,3).map((el:TProduct, _i:number) => <ProductCard item={el} key={_i}/>)}
         </div>
-  </div>*/}
+  </div>*/
     </section>
   );
 };
